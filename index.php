92be9405a760e7be1156876c25a3de7893d68c76
fiwscadet <50857528+fiwscadet@users.noreply.github.com>
2019-05-23 14:08:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,15 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-// Token
 $channel_token =
 'wXRlD4XPs+gtQhhgxLwLcekzU7cpGS0L7DuwT/Rij53yF4r3KGKijL2iWAfnzMdCYk+amyUoVpA9iG8FtokCGTHuUqSgnjT7rvHG6ExH7jQbfMfLvaMSVgebWYHdXckEUJsRZhF8E+93/b4ZZJzKkwdB04t89/1O/w1cDnyilFU=';
 $channel_secret ='5b59e47c3d17b45ae8449d3c52bd3830';
 
 // Get message from Line API
-
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
-
 // Loop through each event
 foreach ($events['events'] as $event) {
 
